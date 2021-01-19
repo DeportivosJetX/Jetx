218942eb1fbedbf492d07ef6377888b1d8400632
jmsr06 <jennysantamria06@gmail.com>
2021-01-19 00:04:34 -0500

    informes vistas

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="footer">
+    <div class="descripcion">
+        <h2>JetX significa ser cool y hacerse notar</h2>
+    </div>
+    <div class="nosotros">
+        <h2>NOSOTROS</h2>
+        <p>Email: jetX@jetx.com</p>
+        <p><a href="">Términos y condiciones</a></p>
+        <p><a href="">Política de datos</a></p>
+    </div>
+    <div class="derechos">
+        <div class="reservados">
+            <i class="fas fa-copyright"></i> Copyright 2021<span> Deportivos JetX </span>todos los derechos reservados
+        </div>
+    </div>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,75 @@
+<header class="header">
+    <div class="banner">
+        <div class="banner__logo">
+            <a href="" class="logo__link">
+                <img class="logo" src="{{asset('../storage/images/JetX.png')}}" alt="">
+                <h1>Deportivos JetX</h1>
+                
+            </a>
+        </div>
+        <div class="banner__iconos">
+            <div class="icono__search">
+                <a id="link-buscar" class="fas-link" href="#"><i class="fas fa-search"></i></a>
+            </div>
+            <div class="icono__user iniciar-sesion">
+                <a class="fas-link" href="#login_side"><i class="fas fa-user"></i></a>
+            </div>
+            <div class="icono__car">
+                <a id="link-carrito" class="fas-link" href="" title="Ver su carrito de compras">
+                    <i class="fas fa-shopping-cart"></i>
+                    <span class="">0</span>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <nav class="menu-desktop">
+     <ul class="menu-desktop__list menu-desktop__firstlevel">
+            <li class="list-item "><a class="list-item__link home" href=""><i class="fas fa-home"></i><span>HOME</span></a></li>
+            {{-- @if(session()->has('categoria')) --}}
+            <li class="list-item "><a class="list-item__li.index') }}"><span>Patinaje</span></a>
+                <ul class="menu-desktop__list categorias menu-desktop__secondlevel">
+            {{-- @forelse(session('categoria') as $categoria) --}}
+                <li class="list-item list-item-sub cateoria"><a class="list-item__li.indexCategoria',$categoria) }}"><span></span></a></li>
+            {{-- @empty --}}
+                <li class="list-item list-item-sub cateoria"><a class="list-item__link" href="#"><span>No hay categorías</span></a></li>
+            {{-- @endforelse --}}
+                </ul>
+            </li>
+            {{-- @endif --}}
+            {{-- @if(session()->has('categoria')) --}}
+            <li class="list-item "><a class="list-item__li.index') }}"><span>Fútbol</span></a>
+                <ul class="menu-desktop__list categorias menu-desktop__secondlevel">
+            {{-- @forelse(session('categoria') as $categoria) --}}
+                <li class="list-item list-item-sub cateoria"><a class="list-item__li.indexCategoria',$categoria) }}"><span></span></a></li>
+            {{-- @empty --}}
+                <li class="list-item list-item-sub cateoria"><a class="list-item__link" href="#"><span>No hay categorías</span></a></li>
+            {{-- @endforelse --}}
+                </ul>
+            </li>
+            {{-- @endif --}}
+            {{-- @if(session()->has('categoria')) --}}
+            <li class="list-item "><a class="list-item__li.index') }}"><span>Baloncesto</span></a>
+                <ul class="menu-desktop__list categorias menu-desktop__secondlevel">
+            {{-- @forelse(session('categoria') as $categoria) --}}
+                <li class="list-item list-item-sub cateoria"><a class="list-item__li.indexCategoria',$categoria) }}"><span></span></a></li>
+            {{-- @empty --}}
+                <li class="list-item list-item-sub cateoria"><a class="list-item__link" href="#"><span>No hay categorías</span></a></li>
+            {{-- @endforelse --}}
+                </ul>
+            </li>
+            {{-- @endif --}}
+            {{-- @if(session()->has('categoria')) --}}
+            <li class="list-item "><a class="list-item__li.index') }}"><span>Tennis</span></a>
+                <ul class="menu-desktop__list categorias menu-desktop__secondlevel">
+            {{-- @forelse(session('categoria') as $categoria) --}}
+                <li class="list-item list-item-sub cateoria"><a class="list-item__li.indexCategoria',$categoria) }}"><span></span></a></li>
+            {{-- @empty --}}
+                <li class="list-item list-item-sub cateoria"><a class="list-item__link" href="#"><span>No hay categorías</span></a></li>
+            {{-- @endforelse --}}
+                </ul>
+            </li>
+            {{-- @endif --}}
+        </ul> 
+    </nav>
+</header>
