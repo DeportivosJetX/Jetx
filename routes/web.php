04053975365aa35107a649d3b9f8e0f6503e86c3
Alexis3004 <nelsonalexiscacerzcarreo37@gmail.com>
2021-01-19 01:48:37 -0500

    modificacion rutas web

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/informes/ingresos', function () {
     return view('ingresos');
+});
+
+Route::get('/informes/rotacion', function () {
+    return view('rotacion');
 });
